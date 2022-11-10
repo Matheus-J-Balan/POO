85ac9d621f01ec3a005bf2474a3a7006a52a8f59
Matheus-J-Balan <matheusbalanj5@gmail.com>
2022-11-10 11:29:51 -0300

    Commit da Pasta POO_Abstracao

---
+++ POO_Abstracao/Program.cs
@@ -0,0 +1,13 @@
+﻿using POO_Abstracao.Models;
+
+Pessoa pessoa1 = new Pessoa();
+
+pessoa1.nome = "Matheus";
+pessoa1.idade = 19;
+pessoa1.Apresentar();
+
+Pessoa pessoa2 = new Pessoa();
+
+pessoa2.nome = "Carlos";
+pessoa2.idade = 53;
+pessoa2.Apresentar();
