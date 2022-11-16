5d0184c0dba832a3b717ee9da41cca239624ef56
Matheus Balan <matheusbalanj5@gmail.com>
2022-11-16 11:44:55 -0300

    Polimorfismo em tempo de execução

---
+++ POO_Poli_Tempo_Execucao/Models/Calculadora.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POO_Poli_Tempo_Execucao.Models
+{
+    public class Calculadora
+    {
+        public int Somar(int num1, int num2){
+            return num1 + num2;
+        }
+
+        public int Somar(int num1, int num2, int num3){
+            return num1 + num2 + num3;
+        }
+    }
+}
---
+++ POO_Poli_Tempo_Execucao/Program.cs
@@ -0,0 +1,7 @@
+﻿using POO_Poli_Tempo_Execucao.Models;
+
+Calculadora calc = new Calculadora();
+
+Console.WriteLine(calc.Somar(10, 20));
+
+Console.WriteLine(calc.Somar(10, 20, 70));
