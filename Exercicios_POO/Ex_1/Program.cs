71d628fed7d44c4cd3901a607b5d881e074a5b46
Matheus Balan <matheusbalanj5@gmail.com>
2022-11-21 10:36:00 -0300

    Ex1

---
+++ Exercicios_POO/Ex_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using Ex_1.Models;
+
+Cachorro c = new Cachorro();
+
+c.raca = "Pastor Alemão";
+c.tam = 1.75;
+c.EmitirSom();
+
+Gato g = new Gato();
+
+g.raca = "Gatito";
+g.tam = 0.55;
+g.EmitirSom();
+
+Passaro p = new Passaro();
+
+p.raca = "Canario";
+p.tam = 0.30;
+p.EmitirSom();
